34e2cc8b7820073d6b7cb6484a0e914ba59b478b
RoberthFurtadoDev <roberthfurtadodev@outlook.com>
2025-07-25 11:45:49 -0300

    escalabilidade-springboot

---
+++ src/main/java/dio/springboot/EscalabilidadeSpringbootApplication.java
@@ -0,0 +1,13 @@
+package dio.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscalabilidadeSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscalabilidadeSpringbootApplication.class, args);
+	}
+
+}
